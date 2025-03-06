ed6b1b2a325ab5651d39d1b752991581029fc1a2
avamus <lukassys2004@gmail.com>
2025-03-06 23:26:24 +0000

    Update ProgressBar.tsx

--- ProgressBar.tsx
+++ ProgressBar.tsx
@@ -6,7 +6,7 @@ interface ProgressBarProps {
 
 export function ProgressBar({ progress }: ProgressBarProps) {
   return (
-    <div className="relative w-full h-3 bg-[#fff5e1] rounded-full overflow-hidden">
+    <div className="relative w-full h-2 bg-[#fff5e1] rounded-full overflow-hidden">
       <div 
         className="absolute left-0 top-0 h-full bg-[#f8b922] transition-all duration-500"
         style={{ width: `${progress}%` }}
